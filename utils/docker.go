90e050e6eb6ee4aa844df0e876c08a099b5de183
Valentin Semenov <valsemenov@mail.ru>
2021-07-08 00:26:14 +0300

    Тестовый вариант

---
+++ utils/docker.go
@@ -0,0 +1,29 @@
+package utils
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+)
+
+func Docker() {
+	commands := []string{//Пока локально. Собираем исходник внутри контейнера. Исполняемый файл запускаем в другом контейнере.
+		"docker rm --force gibdd", //Удаляем контейнер
+		"docker rmi --force gibdd_inage:v1",     //Удаляем изображение
+		"docker system prune -a -f",
+		"GOOS=linux go build -o ./gibdd ./main.go",
+		"make base",
+	}
+	// log.Println("Собираем архив")
+	// Собираем архив
+	for _, command := range commands {
+		cmd := exec.Command("bash", "-c", command)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			err := fmt.Errorf("команда: %v Ошибка: %v", command, err)
+			log.Println(err)
+		}
+	}
+}
