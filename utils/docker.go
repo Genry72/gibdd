7ba98c47bd7cd4d7c80d5664d478d85d26c54c6d
Valentin Semenov <valsemenov@mail.ru>
2021-07-11 20:43:30 +0300

    Форматирование

--- utils/docker.go
+++ utils/docker.go
@@ -10,32 +10,31 @@ import (
 func Docker(command string) {
 	var commands []string
 	if command == "install" {
-		commands = []string{//Компилируем исходник внутри контейнера. Исполняемый файл запускаем в другом контейнере.
+		commands = []string{ //Компилируем исходник внутри контейнера. Исполняемый файл запускаем в другом контейнере.
 			"mkdir ./db",
 			"docker rm --force -v gibdd", //Удаляем контейнер и его вольюм
-			"docker rmi $(docker images | grep gibdd_image | awk '{print $3}')",     //Удаляем изображение
-			"docker rmi $(docker images | grep gibdd_base_image | awk '{print $3}')",     //Удаляем ,базовое изображение
+			"docker rmi $(docker images | grep gibdd_image | awk '{print $3}')",      //Удаляем изображение
+			"docker rmi $(docker images | grep gibdd_base_image | awk '{print $3}')", //Удаляем ,базовое изображение
 			"make install", //Создаем базовый образ
 		}
 	}
 	if command == "update" {
-		commands = []string{//Компилируем исходник внутри контейнера. Исполняемый файл запускаем в другом контейнере.
+		commands = []string{ //Компилируем исходник внутри контейнера. Исполняемый файл запускаем в другом контейнере.
 			"mkdir ./db",
 			"docker rm --force -v gibdd", //Удаляем контейнер
-			"docker rmi $(docker images | grep gibdd_image | awk '{print $3}')",     //Удаляем изображение
+			"docker rmi $(docker images | grep gibdd_image | awk '{print $3}')", //Удаляем изображение
 			"make update", //обновляем бинарник в базовом образе
 			// "docker system prune -a -f",
 		}
 	}
 	if command == "yandexDisk" {
-		commands = []string{//Компилируем исходник внутри контейнера. Исполняемый файл запускаем в другом контейнере.
-			"docker rm --force -v yandexdisk", //Удаляем контейнер
-			"docker rmi $(docker images | grep yandexdisk_image | awk '{print $3}')",     //Удаляем изображение
+		commands = []string{ //Компилируем исходник внутри контейнера. Исполняемый файл запускаем в другом контейнере.
+			"docker rm --force -v yandexdisk",                                        //Удаляем контейнер
+			"docker rmi $(docker images | grep yandexdisk_image | awk '{print $3}')", //Удаляем изображение
 			"make yandexDisk", //обновляем бинарник в базовом образе
 			// "docker system prune -a -f",
 		}
 	}
-
 
 	// Собираем архив
 	for _, command := range commands {
