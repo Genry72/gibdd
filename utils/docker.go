3b3a1fc9366792c691e0a1edc39015481c7b17ce
Valentin Semenov <valsemenov@mail.ru>
2021-07-11 21:06:11 +0300

    Добавил сборку конфига yandex диска из переменных

--- utils/docker.go
+++ utils/docker.go
@@ -29,9 +29,16 @@ func Docker(command string) {
 	}
 	if command == "yandexDisk" {
 		commands = []string{ //Компилируем исходник внутри контейнера. Исполняемый файл запускаем в другом контейнере.
+			"mkdir yandex-disk-config", //Создаем папку с конфигом для подключения к диску
+			"echo $iidYandex > ./yandex-disk-config/iid",
+			"echo $passwdYandex > ./yandex-disk-config/passwd",
+			"echo auth=\"/home/node/.config/yandex-disk/passwd\" > ./yandex-disk-config/config.cfg",
+			"echo dir=\"/yadisk\" >> ./yandex-disk-config/config.cfg",
+			"echo proxy=\"no\" >> ./yandex-disk-config/config.cfg",
 			"docker rm --force -v yandexdisk",                                        //Удаляем контейнер
 			"docker rmi $(docker images | grep yandexdisk_image | awk '{print $3}')", //Удаляем изображение
 			"make yandexDisk", //обновляем бинарник в базовом образе
+			"rm -rf ./yandex-disk-config",
 			// "docker system prune -a -f",
 		}
 	}
